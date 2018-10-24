9b002cc26e6474b51179f1bbd13e87afac74aa86
codytheking <codytheking93@gmail.com>
2018-10-23 23:05:36 -0700

    set default value for reimbursed to get rid of error when creating order and setting vendor

--- db/migrations/015_add_reimbursed_to_orders.rb
+++ db/migrations/015_add_reimbursed_to_orders.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     alter_table(:orders) do
-      add_column :reimbursed, Integer, :null => false
+      add_column :reimbursed, Integer, :null => false, :default => 0
     end
   end
 end
