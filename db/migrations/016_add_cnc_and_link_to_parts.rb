c6d525b52b08692f0b94bd48e1858d32f093a73a
codytheking <codytheking93@gmail.com>
2018-11-01 11:50:29 -0700

    Add row for whether gcode has been created

--- db/migrations/016_add_cnc_and_link_to_parts.rb
+++ db/migrations/016_add_cnc_and_link_to_parts.rb
@@ -2,6 +2,7 @@ Sequel.migration do
   change do
     alter_table(:parts) do
       add_column :gcode_link, String, :null => false
+      add_column :gcode_created, String, :null => false
       add_column :drawing_link, String, :null => false
       add_column :cnc_part, Integer, :null => false
     end
