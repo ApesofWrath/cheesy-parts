551757808b24e005fb8721209287ac16f944716a
codytheking <codytheking93@gmail.com>
2018-11-29 14:43:38 -0800

    Add columns for start date and notes

--- db/migrations/019_create_tasks.rb
+++ db/migrations/019_create_tasks.rb
@@ -6,9 +6,11 @@ Sequel.migration do
       Integer :project_id, :null => false
       String :sub_name, :null => false
       Date :deadline, :null => false
+      Date :start_date, :null => false
       String :milestone_id, :null => false
       String :assignee, :null => false
       String :status, :null => false
+      String :notes
     end
   end
 end
